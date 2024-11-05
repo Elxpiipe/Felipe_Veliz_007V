36ea7fa1ef2676d6590d03a9ee8c5e2b888e9820
Felipe Veliz <fel.veliz@duocuc.cl>
2024-11-05 14:16:33 -0300

    se modifico el servicio para iniciar sesion y se creo el metodo para cerrar sesion

---
+++ src/app/interfaces/UsuarioLogueado.ts
@@ -0,0 +1,9 @@
+export interface UsuarioLogueado  {
+    id: number;
+    username: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    gender: string;
+    accessToken: string;
+}
--- src/app/servicio/auth/auth.service.ts
+++ src/app/servicio/auth/auth.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UsuarioLogueado } from 'src/app/interfaces/UsuarioLogueado';
+import { CuerpoLogin } from 'src/app/interfaces/CuerpoLogin';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private readonly URL_LOGIN: string = 'https://dummyjson.com/auth/login'
+  public usuarioLogueado: UsuarioLogueado | null = null;
+  public accessToken: string | null = null;
+  constructor(
+    private http: HttpClient
+  ) { }
 
-  constructor() { }
+  public iniciarSesion(nombre_usuario: string, contrasenia: string){
+    const cuerpo: CuerpoLogin = {
+      username: nombre_usuario,
+      password: contrasenia
+    }
+    this.http.post<UsuarioLogueado>(this.URL_LOGIN, JSON.stringify(cuerpo),{
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .subscribe(resultado=>{
+      this.usuarioLogueado = resultado;
+      this.accessToken = resultado.accessToken;
+      console.log(resultado);
+      
+    });
+  }
+  
+  public cerrarSesion(){
+    if(this.usuarioLogueado){
+      this.usuarioLogueado = null;
+      this.accessToken = null;
+    }
+  }
 }
